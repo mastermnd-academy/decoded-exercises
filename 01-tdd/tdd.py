2c0357bba25477e494677b9b85ea818e5d13c1d2
mastermndio <abrooks@mastermnd.io>
2021-01-26 23:38:55 +0000

    exercises addes

---
+++ 01-tdd/tdd.py
@@ -0,0 +1,5 @@
+# Write a function that takes in a string and returns its length
+
+# Write a funtion that takes in a list of integers and returns the product of every
+
+# Write a function that takes in a string and returns a new string with only the letters in the first and odd indicies
---
+++ 01-tdd/tdd_test.py
@@ -0,0 +1,11 @@
+import unittest
+
+from tdd import *
+
+class TwoFerTest(unittest.TestCase):
+    #def test_no_name_given(self):
+    #    self.assertEqual(function("Input), "Expected Output")
+
+
+if __name__ == "__main__":
+    unittest.main()
