b34cd08a6298ce486a3634d994b07f5be7dedb94
Aaron B <abrooks@mastermnd.io>
2021-01-27 02:25:46 +0000

    tdd solutions addes

--- 01-tdd/tdd.py
+++ 01-tdd/tdd.py
@@ -1,5 +1,30 @@
 # Write a function that takes in a string and returns its length
+def check_length(our_string=""):
+    return len(our_string)
 
-# Write a funtion that takes in a list of integers and returns the product of every
+# Write a funtion that takes in a list of integers and returns the product of every item in list
+def check_product(our_list=[]):
+    product = 1
 
-# Write a function that takes in a string and returns a new string with only the letters in the first and odd indicies
+    if len(our_list) == 0:
+        return 0
+    else:
+        for num in our_list:
+            product *= num 
+    
+    return product
+
+
+# Write a function that takes in a string and returns a new string with only the letters in the starting letter and all odd indicies
+def odd_string(our_string):
+    result = []
+    if len(our_string) == 0:
+        return ""
+    
+    result.append(our_string[0])
+
+    for letter in range(1,len(our_string), 2):
+           result.append(our_string[letter])
+
+    result = "".join(result)
+    return result
--- 01-tdd/tdd_test.py
+++ 01-tdd/tdd_test.py
@@ -4,8 +4,39 @@ from tdd import *
 
 class TwoFerTest(unittest.TestCase):
     #def test_no_name_given(self):
-    #    self.assertEqual(function("Input), "Expected Output")
+    #    self.assertEqual(function("Input"), "Expected Output")
 
+    def test_check_string_length(self):
+        self.assertEqual(check_length("Hello"), 5)
+        self.assertEqual(check_length("Hello World"), 11)
+        self.assertEqual(check_length("https://github.com/mastermnd-academy/decoded-exercises.git"), 58)
+    
+    def test_check_empty_value(self):
+        self.assertEqual(check_length(), 0)
+        self.assertEqual(check_length(""), 0)
+
+    def test_check_product(self):
+        self.assertEqual(check_product([1,1,5]), 5)
+    
+    def test_check_multiply_by_zero(self):
+        self.assertEqual(check_product([10, 0]), 0)
+        self.assertEqual(check_product([0, 10]), 0)
+        self.assertEqual(check_product([1,1,5, 392, 67, 0, 2827, 38]), 0)
+
+    def test_check_product_negative_values(self):
+        self.assertEqual(check_product([-1,1,5]), -5)
+        self.assertEqual(check_product([-1,-8]), 8)
+        self.assertEqual(check_product([]), 0)
+
+    def test_odd_string_return(self):
+        self.assertEqual(odd_string("Hello There You"), "Hel hr o")
+        self.assertEqual(odd_string("Yes"), "Ye")
+        self.assertEqual(odd_string(""), "")
+        self.assertEqual(odd_string("        "), "     ")
+
+        
+        
+        
 
 if __name__ == "__main__":
     unittest.main()
